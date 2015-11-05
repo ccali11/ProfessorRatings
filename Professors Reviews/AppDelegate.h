0942e101b389f1cd648e814bf7234e415303ce68
Christopher Cali <christophercali@Christophers-MacBook-Pro.local>
2015-11-04 19:37:41 -0500

    Initial Commit

---
+++ Professors Reviews/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Professors Reviews
+//
+//  Created by Christopher Cali on 11/4/15.
+//  Copyright (c) 2015 CJC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Professors Reviews/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Professors Reviews
+//
+//  Created by Christopher Cali on 11/4/15.
+//  Copyright (c) 2015 CJC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
