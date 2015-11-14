92a353632268dfd8efa071bd36c8e56732839dbc
Christopher Cali <christophercali@christophers-macbook-pro.local>
2015-11-14 10:10:19 -0500

    Created Navigation Controller, Segue from Courses to Professors.

--- Professors Reviews/SelectCourseViewController.m
+++ Professors Reviews/SelectCourseViewController.m
@@ -6,35 +6,58 @@
 //  Copyright (c) 2015 CJC. All rights reserved.
 //
 
+// Importing subclass to NSSomething
 #import "SelectCourseViewController.h"
 
+//Calling interface UIViewController class to interact with implementation and serve as infrastructure
 @interface SelectCourseViewController () <UITableViewDataSource, UITableViewDelegate>
 
 @end
 
 @implementation SelectCourseViewController
 
--(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+//TableView Data Source
+
+//Declaring number of rows in table
+-(NSInteger)tableView:(UITableView *)tableView
+numberOfRowsInSection:(NSInteger)section
 {
     return 5;
 }
 
--(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+//This function builds the cells
+-(UITableViewCell *)tableView:(UITableView *)tableView
+        cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
+    //Declares a variable, calls it myCell, asks if there is a cell with the reuse identifier (Course & Prof)
     UITableViewCell * myCell = [tableView dequeueReusableCellWithIdentifier:@"CourseAndProfCell"];
     if (!myCell) {
-        myCell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:@"CourseAndProfCell"];
+        myCell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle
+                                        reuseIdentifier:@"CourseAndProfCell"];
     }
+
+//Creating fields for table
     myCell.textLabel.text = @"Professor";
     myCell.detailTextLabel.text = @"Course ID";
     return myCell;
 }
 
+//I think this is to remind app that where the user is with respect to its starting location within the app
 -(NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
 {
     return 1;
 }
 
+//End TableView Data Source
+
+
+//Beginning TableView Delegate
+-(void)tableView:(UITableView *)tableView
+didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    [self performSegueWithIdentifier:@"CourseList to Prof"
+                              sender:self];
+}
 
 
 @end
