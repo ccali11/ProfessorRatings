68d83e224ea77e5a6b8a122dd0077546d589c6be
Christopher Cali <christophercali@christophers-macbook-pro.local>
2015-11-14 11:21:58 -0500

    Created Tray as well as Back and Next Functionality

---
+++ Professors Reviews/RateProfViewController.h
@@ -0,0 +1,13 @@
+//
+//  RateProfViewController.h
+//  Professors Reviews
+//
+//  Created by Christopher Cali on 11/14/15.
+//  Copyright © 2015 CJC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RateProfViewController : UIViewController
+
+@end
---
+++ Professors Reviews/RateProfViewController.m
@@ -0,0 +1,31 @@
+//
+//  RateProfViewController.m
+//  Professors Reviews
+//
+//  Created by Christopher Cali on 11/14/15.
+//  Copyright © 2015 CJC. All rights reserved.
+//
+
+#import "RateProfViewController.h"
+
+@interface RateProfViewController ()
+
+@property (weak, nonatomic) IBOutlet UIView *tray;
+
+@end
+
+@implementation RateProfViewController
+- (IBAction)buttonPressed:(UIButton *)sender {
+    if ([sender.titleLabel.text isEqualToString: @"Back"]) {
+        self.tray.center = CGPointMake(self.tray.center.x + self.tray.frame.size.width/3, self.tray.center.y);
+    }
+    
+    else if ([sender.titleLabel.text isEqualToString:@"Next"]){
+        self.tray.center = CGPointMake(self.tray.center.x - self.tray.frame.size.width/3, self.tray.center.y);
+        
+    }
+}
+
+
+
+@end
