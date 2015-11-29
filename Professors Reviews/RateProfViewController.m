558e398160457a5e99d2f378d3200265d96bf80d
Christopher Cali <christophercali@christophers-macbook-pro.local>
2015-11-29 11:28:23 -0500

    Added animateForward and animateBack functionality

--- Professors Reviews/RateProfViewController.m
+++ Professors Reviews/RateProfViewController.m
@@ -11,35 +11,78 @@
 @interface RateProfViewController ()
 
 @property (weak, nonatomic) IBOutlet UIView *tray;
-@property (nonatomic) int myint;
+@property (nonatomic) int questionNumber;
+
+@property (nonatomic) int questionCountTotal;
 
 @end
 
 //This is the tray. Animate moving the tray 0.75s
 @implementation RateProfViewController
+
+//View Life Cycle Methods
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.questionCountTotal = 3;
+    self.questionNumber = 1;
+}
+
+
 - (IBAction)buttonPressed:(UIButton *)sender {
-    if ([sender.titleLabel.text isEqualToString: @"Back"]) {
-        [self animateRight];
+    if ([sender.titleLabel.text isEqualToString: @"Back"])
+    
+    {
+        if (![self atLeftBound]) {
+            [self animateBack];
+            self.questionNumber--;
+        }
         
     }
     
-    else if ([sender.titleLabel.text isEqualToString:@"Next"]){
-        [self animateLeft];
+    else if ([sender.titleLabel.text isEqualToString:@"Next"])
+    {
+        if (![self atRightBound]) {
+            [self animateForward];
+            self.questionNumber++;
+        }
     }
+    
+    
 }
 
-- (void) animateLeft {
+- (void) animateForward {
     [UIView animateWithDuration:0.75
                      animations:^(void){
                          self.tray.center = CGPointMake(self.tray.center.x - self.tray.frame.size.width/3, self.tray.center.y);
                      }];
 }
 
-- (void) animateRight {
+- (void) animateBack {
     [UIView animateWithDuration:0.75
                      animations:^(void){
                          self.tray.center = CGPointMake(self.tray.center.x + self.tray.frame.size.width/3, self.tray.center.y);
                      }];
+}
+
+- (BOOL) atLeftBound {
+    
+    if (self.questionNumber <= 1) {
+        return YES;
+    }  else {
+        return NO;
+    }
+    
+}
+
+- (BOOL) atRightBound {
+    
+    if (self.questionNumber >= self.questionCountTotal) {
+        return YES;
+    }
+    else {
+        return NO;
+    }
+    
 }
 
 //Question 1 label
