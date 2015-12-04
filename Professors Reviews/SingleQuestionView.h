04243806ac329a2a9694273145ae1971a3df37a5
Christopher Cali <christophercali@christophers-macbook-pro.local>
2015-12-03 20:26:35 -0500

    Built xib file for SingleQuestionView. It will be IBInspectable

---
+++ Professors Reviews/SingleQuestionView.h
@@ -0,0 +1,13 @@
+//
+//  SingleQuestionView.h
+//  Professors Reviews
+//
+//  Created by Christopher Cali on 12/3/15.
+//  Copyright © 2015 CJC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SingleQuestionView : UIView
+
+@end
---
+++ Professors Reviews/SingleQuestionView.m
@@ -0,0 +1,15 @@
+//
+//  SingleQuestionView.m
+//  Professors Reviews
+//
+//  Created by Christopher Cali on 12/3/15.
+//  Copyright © 2015 CJC. All rights reserved.
+//
+
+#import "SingleQuestionView.h"
+
+@implementation SingleQuestionView
+
+
+
+@end
